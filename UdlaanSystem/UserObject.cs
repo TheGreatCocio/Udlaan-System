8518c07622f5b1d3eb61188c4df9586dfad877e0
Mikkel Rasmussen <zbcmiras17@zbc.dk>
2017-08-08 10:28:04 +0200

    UserObject Done!

--- UdlaanSystem/UserObject.cs
+++ UdlaanSystem/UserObject.cs
@@ -8,5 +8,26 @@ namespace UdlaanSystem
 {
     class UserObject
     {
+        public UserObject(string fName, string lName, string zbcName, string userMifare, int phoneNumber, bool isTeacher, bool hasPC, bool isDisabled)
+        {
+            fName = this.fName;
+            lName = this.lName;
+            zbcName = this.zbcName;
+            userMifare = this.userMifare;
+            phoneNumber = this.phoneNumber;
+            isTeacher = this.isTeacher;
+            hasPC = this.hasPC;
+            isDisabled = this.isDisabled;
+
+        }
+
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public string zbcName { get; set; }
+        public string userMifare { get; set; }
+        public int phoneNumber { get; set; }
+        public bool isTeacher { get; set; }
+        public bool hasPC { get; set; }
+        public bool isDisabled { get; set; }
     }
 }
