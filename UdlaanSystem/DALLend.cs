cfd25a4638d1354667a502555882d9c2ebbbc331
mark fog pedersen <mark1055@live.dk>
2017-08-10 09:28:39 +0200

    10/8-17 09:30

--- UdlaanSystem/DALLend.cs
+++ UdlaanSystem/DALLend.cs
@@ -10,5 +10,46 @@ namespace UdlaanSystem
 {
     class DALLend
     {
+        public DALLend()
+        {
+        }
+        private static DALLend instance;
+
+        public static DALLend Instance
+        {
+            get
+            {
+                if (instance == null)
+                { instance = new DALLend(); }
+                return instance;
+            }
+        }
+
+        private string sqlConn;
+        private MySqlConnection MysqlConnection = null;
+
+        private void ConnectMySql()
+        {
+            sqlConn = @"server=10.108.48.19; Database=supply_ri; User Id=udlaan; Password=RFIDrules; integrated security=false";
+
+            if (MysqlConnection == null)
+            {
+                MysqlConnection = new MySqlConnection(sqlConn);
+            }
+            try
+            {
+                MysqlConnection.Open();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("####################Failed to connect to sql server: " + ex);
+            }
+        }
+
+        public string GetLendedByItemMifare (string itemMifare)
+        {
+            
+        }
     }
+
 }
