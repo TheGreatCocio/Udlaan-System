0294ff988a2c03f172ad603fecacbaa90a639dc1
NAVNEBESKYTTET NAVNEBESKYTTET (zbcafafv164) <zbcafafv164@zbc.dk>
2017-09-07 10:26:34 +0200

    asdfghjklæ

--- UdlaanSystem/Settings1.Designer.cs
+++ UdlaanSystem/Settings1.Designer.cs
@@ -25,7 +25,7 @@ namespace UdlaanSystem {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool SmsService {
             get {
                 return ((bool)(this["SmsService"]));
